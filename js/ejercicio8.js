d64714d70d5ef7c37df1d33c2a5efa62f1c5de42
Santiago Puertas <santidoors13@gmail.com>
2024-04-20 19:55:02 -0300

    Terminado ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,17 @@
+/* 8- Crea una clase llamada "Persona" que tenga las propiedades "nombre", "edad" y "profesión", y los métodos "saludar" y "despedirse". Luego, crea dos objetos de la clase "Persona" con diferentes valores para sus propiedades y llama a sus métodos "saludar" y "despedirse". */
+
+class Persona {
+  constructor(nombre, edad, profesion) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.profesion = profesion;
+  }
+
+  saludar() {
+    alert(`${this.nombre} te saluda`);
+  }
+
+  despedir() {
+    alert(`${this.nombre} se despide`);
+  }
+}
